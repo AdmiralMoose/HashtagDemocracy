834fdd986d7bc1b5ce3b1b2e8557716b4d4f0935
Jordan Dalton <jordan.8474@gmail.com>
2014-11-25 22:06:46 +0000

    Update HashtagDemocracy.java

--- src/hashtagdemocracy/HashtagDemocracy.java
+++ src/hashtagdemocracy/HashtagDemocracy.java
@@ -1,15 +1,37 @@
 
-
 package hashtagdemocracy;
 
+import hashtagdemocracy.monthsHandler.TimeExpired;
+import hashtagdemocracy.monthsHandler.TimerThread;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-public final class HashtagDemocracy extends JavaPlugin{
+/**
+ *
+ * @author baileypellow
+ */
+public final class HashtagDemocracy extends JavaPlugin implements Listener{
     //When the plan comes out we can sort out who does what.
+	
+	TimerThread thread = new TimerThread();
 
     @Override
     public void onEnable(){
-        
+        thread.start();
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
+    
+    @Override
+    public void onDisable(){
+    	thread.cancel();
+    }
+    
+    @EventHandler
+    public void threeMonths(TimeExpired time){
+    	Bukkit.broadcastMessage("Time has expired, time to vote!");
+    }
+    
 }
