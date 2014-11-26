0e5defe79aaef433b8d6b9217ecefe766e23764a
Jordan Dalton <jordan.8474@gmail.com>
2014-11-26 22:24:27 +0000

    Update HashtagDemocracy.java

--- src/hashtagdemocracy/HashtagDemocracy.java
+++ src/hashtagdemocracy/HashtagDemocracy.java
@@ -15,16 +15,25 @@ public final class HashtagDemocracy extends JavaPlugin{
     //When the plan comes out we can sort out who does what.
 	
 	TimerThread thread = new TimerThread();
-
+	Boolean isVoteDay = false;
+	
     @Override
     public void onEnable(){
         thread.start();
-        Bukkit.getPluginManager().registerEvents( new VotingHandler() , this ); // Register the voting handler
+        Bukkit.getPluginManager().registerEvents( new VotingHandler(this) , this ); // Register the voting handler
     }
     
     @Override
     public void onDisable(){
     	thread.cancel();
+    }
+    
+    public boolean canVote(){
+    	return isVoteDay;
+    }
+    
+    public void setCanVote(Boolean newVal){
+    	isVoteDay = newVal;
     }
     
 }
