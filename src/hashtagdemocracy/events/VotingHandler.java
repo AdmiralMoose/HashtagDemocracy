a626ad74fd0c563a537a00848cb159eda9ee5078
Jordan Dalton <jordan.8474@gmail.com>
2014-11-26 09:22:49 +0000

    Create VotingHandler.java

---
+++ src/hashtagdemocracy/events/VotingHandler.java
@@ -0,0 +1,25 @@
+package hashtagdemocracy.events;
+
+import org.bukkit.event.Listener;
+
+public class VotingHandler extends Listener{
+  
+  @EventHandler
+  public void endElectoral(TimeExpired event){
+    // End the current electoral code here...
+    System.out.println("Ending the current electoral..");
+  }
+  
+  @EventHandler
+  public void startVoting(StartVoting event){
+    // Allow the players to start voting
+    System.out.println("Allowing the players to vote for one day..");
+  }
+  
+  @EventHandler
+  public void endVoting(EndVoting event){
+    // Don't allow the players to vote anymore
+    System.out.println("Players can no longer vote..");
+  }
+  
+}
