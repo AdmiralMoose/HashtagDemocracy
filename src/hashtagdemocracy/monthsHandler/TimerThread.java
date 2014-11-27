4ffc8a1aa10858bc14ef4e39bb7c3ea4f99c508e
Jordan Dalton <jordan.8474@gmail.com>
2014-11-27 09:57:29 +0000

    Update TimerThread.java
    Changed timer from 1 MC day to 1 REAL day.

--- src/hashtagdemocracy/monthsHandler/TimerThread.java
+++ src/hashtagdemocracy/monthsHandler/TimerThread.java
@@ -7,26 +7,28 @@ import hashtagdemocracy.events.TimeExpired;
 import org.bukkit.Bukkit;
 
 public class TimerThread extends Thread{
+	
+	int days = 0;
 		
 	/**
 	 * Timer thread is to be started with the plugin
 	 * This thread will always run in the background
 	 * Everytime 3 months (In minecraft) passes
 	 * it will call {@link TimeExpired}
-	 */
+	 */ 
 	public TimerThread(){
 	}
 	
 	public void run(){
 		try{
-			int days = 0;
+			
 			while ( ! Thread.currentThread().isInterrupted() ){
-				Thread.sleep( (1000 * 60) *20 ); // Sleep for 20 mins ( 1 MC Day)
+				Thread.sleep( ((1000 * 60) *60) *24 ); // Sleep for 1 REAL day -> MC day = (1000 * 60) *20
 				days++;
 				if ( days == (31 *3) ){
 					endElectoral(); // End the current peoples' roles to start anew
 					startVotes(); // Allow the players to votr
-					Thread.sleep( (1000 * 60) *20); // Sleep for one MC day
+					Thread.sleep( ((1000 * 60) *60) *24 ); // Sleep for one real day
 					endVoting(); // Don't allow the players to vote anymore
 					days = 0;
 				}else{
